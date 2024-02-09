883aabd3c63acc916905a99d1fe68e523ff3b388
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2024-02-09 11:08:26 +0100

    Install implemented but need to check.

--- PrinterQueue/MainForm.cs
+++ PrinterQueue/MainForm.cs
@@ -10,20 +10,22 @@ namespace PrinterQueue
 {
 	public partial class MainForm : Form
 	{
-		private DataModel _printerInfo;
+		private DataModel _dataModel;
 		private PrinterDrivers _printerDrivers;
 		private GeneratePortName _generatePort;
 		private QueueMgt _queueMgt;
 		private GroupPermission _groupPermission;
+		private InstallMgt _installMgt;
 		
 
 		public MainForm()
 		{
-			_printerInfo = new DataModel();
+			_dataModel = new DataModel();
 			_printerDrivers = new PrinterDrivers();
 			_generatePort = new GeneratePortName();
 			_queueMgt = new QueueMgt();
 			_groupPermission = new GroupPermission();
+			_installMgt = new InstallMgt();
 
 			InitializeComponent();
 
@@ -73,10 +75,10 @@ namespace PrinterQueue
 
 		public void GroupPermission()
 		{
-			if (_printerInfo != null)
+			if (_dataModel != null)
 			{
-				string printerName = _printerInfo.PrinterName;
-				string printServerName = _printerInfo.SystemName;
+				string printerName = _dataModel.PrinterName;
+				string printServerName = _dataModel.SystemName;
 				string userNameOrGroup = txtGroups.Text;
 
 				_groupPermission.SetPrinterPermissions(printerName, printServerName, userNameOrGroup);
@@ -104,24 +106,52 @@ namespace PrinterQueue
 		{
 			try
 			{
-				ReadPortName();
-				//_printerInfo.PortName = textPortName.Text;
-				_printerInfo.PortAddress = txtPortAddress.Text;
-				//_printerInfo.PrinterQueue = txtPrinterQueue.Text;
-				PrinterQueue();
-				_printerInfo.Comment = txtComment.Text;
-				_printerInfo.Location = txtLocation.Text;
-				//_printerInfo.Groups = txtGroups.Text;
-				GroupPermission();
+				string userInputPortName = txtPortName.Text;
+				if (!_generatePort.IsPortNameAvailable(userInputPortName))
+				{
+					ReadPortName();
+				}
+				else
+				{
+					_dataModel.PortName = userInputPortName;
+				}
+
+				_dataModel.PortAddress = txtPortAddress.Text;
+
+				string userInputPrinterQueue = txtPrinterQueue.Text;
+				if (!_queueMgt.PrinterQueueExists(userInputPrinterQueue))
+				{
+					PrinterQueue();
+				}
+				else
+				{
+					_dataModel.PrinterQueue = userInputPrinterQueue;
+				}
+
+				_dataModel.Comment = txtComment.Text;
+				_dataModel.Location = txtLocation.Text;
 
 				string selectedDriverInfo = comboDrivers.SelectedItem?.ToString();
 				if (selectedDriverInfo != null)
 				{
 					string driverName = selectedDriverInfo.Substring(selectedDriverInfo.IndexOf(":") + 2);
-					_printerInfo.DriverName = driverName;
-				}
-				//Installation on going
+					_dataModel.DriverName = driverName;
 
+					if (_installMgt.PrinterExists(_dataModel.PrinterName))
+					{
+						MessageBox.Show($"Printer '{_dataModel.PrinterName}' is already installed.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					}
+					else
+					{
+						_installMgt.ExecutePowerShellScript($"Add-Printer -Name '{_dataModel.PrinterName}' -DriverName '{_dataModel.DriverName}' -PortName '{_dataModel.PortName}' -Comment '{_dataModel.Comment}' -Location '{_dataModel.Location}'");
+
+						MessageBox.Show($"Printername: {_dataModel.PrinterName} has been installed. PortAddress: {_dataModel.PortAddress}, PortName: {_dataModel.PortName}, Location: {_dataModel.Location}");
+					}
+				}
+				else
+				{
+					MessageBox.Show("Please select a driver.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
 			}
 			catch (ApplicationException ex)
 			{
